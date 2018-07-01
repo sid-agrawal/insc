f555f0058ab2dac518326d92be19a29efb092d8a
Sid Agrawal <sid@arista.com>
2018-07-01 02:57:24 -0700

    Add libs which might be needed eventually

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/muka/go-bluetooth/bluez"
+	"github.com/paypal/gatt"
 	"github.com/pebbe/zmq4"
 )
 
 func main() {
 	fmt.Println("vim-go")
 	_ = zmq4.NewPoller()
+	_ = gatt.CharNotify
+	_ = bluez.FlagCharacteristicRead
 }
