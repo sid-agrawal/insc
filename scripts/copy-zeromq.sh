7f1607ab8bbe64bdffdbce0284ab18607be325ef
Sid Agrawal <sid@arista.com>
2018-07-07 12:08:39 -0700

    Use master of insc-3rd-party-libs for now

--- scripts/copy-zeromq.sh
+++ scripts/copy-zeromq.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Download zeromq from 3rd-party-libs
-git clone -b 'v4.2.5' --single-branch --depth 1 https://github.com/sid-agrawal/insc-3rd-party-libs
+#git clone -b 'v4.2.5' --single-branch --depth 1 https://github.com/sid-agrawal/insc-3rd-party-libs
+git clone https://github.com/sid-agrawal/insc-3rd-party-libs
 
-# copy to correct location
+# copy lib to correct location
 sudo cp ./insc-3rd-party-libs/libzmq.so.5.1.5 /usr/local/lib/
 sudo ln -s libzmq.so.5.1.5 /usr/local/lib/libzmq.so.5
 sudo ln -s libzmq.so.5.1.5 /usr/local/lib/libzmq.so
+
+# copy header file to correct locatio
+sudo cp ./insc-3rd-party-libs/zmq.h /usr/local/include/
+sudo cp ./insc-3rd-party-libs/zmq_utils.h /usr/local/include/
 
 # Install zeromq driver on linux
 sudo ldconfig
