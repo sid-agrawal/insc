a4e9216b349c16971e8d63e65399542776d2461d
Sid Agrawal <sid@arista.com>
2018-07-07 12:21:21 -0700

    Move steps from copy-zeromq.sh insc-3rd-party-libs repo

--- scripts/copy-zeromq.sh
+++ scripts/copy-zeromq.sh
@@ -5,22 +5,7 @@
 git clone https://github.com/sid-agrawal/insc-3rd-party-libs
 
 # copy lib to correct location
-sudo cp ./insc-3rd-party-libs/libzmq.so.5.1.5 /usr/local/lib/
-sudo ln -s libzmq.so.5.1.5 /usr/local/lib/libzmq.so.5
-sudo ln -s libzmq.so.5.1.5 /usr/local/lib/libzmq.so
+cd insc-3rd-party-libs
+sudo ./install.sh
+cd ../
 
-# copy header file to correct locatio
-sudo cp ./insc-3rd-party-libs/zmq.h /usr/local/include/
-sudo cp ./insc-3rd-party-libs/zmq_utils.h /usr/local/include/
-
-# Install zeromq driver on linux
-sudo ldconfig
-
-# Check installed
-ldconfig -p | grep zmq
-
-# Expected
-############################################################
-# libzmq.so.5 (libc6,x86-64) => /usr/local/lib/libzmq.so.5
-# libzmq.so (libc6,x86-64) => /usr/local/lib/libzmq.so
-############################################################
